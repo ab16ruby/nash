80430988e6e0f3ed6a41518d97df22a1631fec28
KUBRA <cankayakubra@hotmail.com>
2016-02-02 16:55:01 +0200

    put tamam

--- ortas.rb
+++ ortas.rb
@@ -10,18 +10,18 @@ class Orta
 	 @isim=nil
 	end
 	def oyunabasla
-		print "Kullanici adi : "
+		@a.yazdir("Kullanici adi : ")
 		isim=gets.strip
 		ran=rand(0..1)
 		if ran==1 
 			 sira=1  
-			 puts "PC basliyor".strip 
+			 @a.yazdir("PC basliyor") 
 		else
 			 sira=2  
-			 puts "#{isim.strip} basliyor".strip
+			  @a.yazdir("#{isim.strip} basliyor")
 		end
 		ran = rand(10...100)
-		puts "Baslanacak sayi #{ran}\n".strip
+		@a.yazdir("Baslanacak sayi #{ran}\n")
 		loop  do
 			if sira==1
 				sira=2
@@ -37,30 +37,30 @@ class Orta
 				    cev=rand(1..3)
 				 end
 				ran-=cev
-				print "PC nin sırası : #{cev}\n"
+				@a.yazdir("PC nin sırası : #{cev}\n")
 				 if ran==1
-				 	puts "PC kazandı!\n".strip
+				 	@a.yazdir("PC kazandı!\n")
 				 	break
 			     end
 			 elsif sira==2	
-				 print "oyuncunun sirasi : "
+				 @a.yazdir("oyuncunun sirasi : ")
 				 loop do
 					cev=gets.to_i
 					if cev >=0 and cev <=3
 						break
 					end
-					puts "Hatali secim!\nTekrar sayı giriniz :".strip
+					@a.yazdir("Hatali secim!\nTekrar sayı giriniz :")
 				  end
 				  ran-=cev
 		          sira=1
 		         if ran==1
-			     puts "Oyuncu #{isim.strip} kazandı!".strip
-			     puts cev
+			     @a.yazdir("Oyuncu #{isim.strip} kazandı!")
+			     @a.yazdir(cev)
 			     break
 		         end
 		     end
  	     end
- 	     puts "Sayının son değeri #{ran}\n"
+ 	     @a.yazdir("Sayının son değeri #{ran}\n")
      end
 end
 oyun=Orta.new()
