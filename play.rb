170a101a39428cddf91ef6f35313060e4623ec9b
serhatko <kserhat94@hotmail.com>
2016-02-02 11:38:31 +0200

    Başlangıç

--- play.rb
+++ play.rb
@@ -1,23 +1,30 @@
 
 a = nil
 b = nil
-
-print "Kim Başlayacak (1 PC), (2 USER):"
-
 f = nil
+print "Kullanici adi : "
+isim=gets
+a=rand(0..1)
 
-loop do
-f = gets.strip
-if(f=="1")
-  a = rand(11..50)
-  break
-elsif(f=="2")
-  loop do
-    puts "a giriş:"
+if a==1 
+	f=1  
+	p "PC basliyor" 
+	a = rand(11..50)
+end
+if a==0 
+	f=2  
+	p "Kullanici basliyor" 
+	puts "a giriş:"
     a = gets.strip.to_i
-    break if (a>11) and (a<100)
-  end
 end
-end
-
+# loop  do
+# 	if f==1
+# 		print "pc nin sırası "
+#		f=2
+# 	end
+#   elsif f==2	
+# 		print "oyuncu sirasi"
+#       f=1
+#  end
+# end
 p a
