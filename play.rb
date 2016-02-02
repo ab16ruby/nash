8f369b8a8ea9b7a7eecef96d41f6eb6f429877f5
serhatko <kserhat94@hotmail.com>
2016-02-02 14:02:26 +0200

    Kontrol eklendi

--- play.rb
+++ play.rb
@@ -20,24 +20,40 @@ ran = rand(10...100)
 puts "Baslanacak sayi #{ran}\n".strip
 loop  do
 	if sira==1
-	print "pc nin sırası "
-	if ran%3==1
-		cev=(1..2)
-	else
-		if (ran-2)%3==1
-			cev=2
-		else 
-			cev=1
-	end
-	ran-=cev
-	sira=2
-	p cev
- 	end
+		print "pc nin sırası "
+			if ran%3==1
+				cev=rand(1..2)
+			else
+				if ran%3==0
+					cev=2
+				else 
+					cev=1
+				end
+			end
+		ran-=cev
+		if ran<=1
+			puts "PC kazandı!\n".strip
+			puts cev
+			break
+		end
+		sira=2
+		puts cev
 	elsif sira==2	
 		print "oyuncu sirasi"
-		cev=gets.to_i
+		loop do
+			cev=gets.to_i
+			if cev==1 or cev==2
+				break
+			end
+			puts "Hatali secim!\nTekrar sayı giriniz".strip
+		end
 		ran-=cev
+		if ran<=1
+			puts "Oyuncu #{isim.strip} kazandı!".strip
+			puts cev
+			break
+		end
        sira=1
   end
-  p ran
+  puts ran
  end
